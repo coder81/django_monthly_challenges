0b939e9c71cc6067cc80051d55bfe3b5c508b290
Coder81 <headleaderscom@gmail.com>
2026-06-09 22:20:34 +0200

    Template inheritance

--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
   "january": "Eat no meat for entire month!",
@@ -14,19 +15,15 @@ monthly_challenges = {
   "september": "Eat no meat for entire month!",
   "october": "Eat no meat for entire month!",
   "november": "Eat no meat for entire month!",
-  "december": "Eat no meat for entire month!"
+  "december": None
 }
 
 # Create your views here.
 def index(request):
-  list_items = ""
   months = list(monthly_challenges.keys())
-  for month in months:
-    capitalized_month = month.capitalize()
-    month_path = reverse("month-challenge", args=[month])
-    list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
-  response_data = f"<ul>{list_items}</ul>"
-  return HttpResponse(response_data)
+  return render(request, "challenges/index.html", {
+    "months": months
+  })
 
 def monthly_challenge_by_number(request,month):
   months = list(monthly_challenges.keys())
@@ -41,7 +38,12 @@ def monthly_challenge_by_number(request,month):
 def monthly_challenge(request, month):
   try:
     challenge_text = monthly_challenges[month]
-    response_data = f"<h2>{challenge_text}</h2>"
+    response_data = render(request, "challenges/challenge.html",{
+      "text": challenge_text,
+      "month_name": month
+    })
+    return HttpResponse(response_data)
   except:
     return HttpResponseNotFound("This month is not supported!")
+  
   return HttpResponse(challenge_text)
